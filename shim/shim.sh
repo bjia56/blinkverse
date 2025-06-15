e19dc8c2c8343b6c76428bcd4f147ef37b88a869
Brett Jia <dev.bjia56@gmail.com>
2025-06-15 16:13:19 -0400

    generalize shims

--- shim/shim.sh
+++ shim/shim.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+me=$(basename $0)
+
 # Initialize an empty array to hold the filtered arguments
 filtered_args=()
 
 # Iterate over all provided arguments
 for arg in "$@"; do
-  # Let zig choose the cpu type
+  # Let compiler choose the cpu type
   if [[ "${arg:0:7}" != "-mtune=" ]]; then
     filtered_args+=("$arg")
   fi
 done
 
 # Execute the compiler with the filtered arguments
-exec zig "${filtered_args[@]}"
+exec $0 "${filtered_args[@]}"
